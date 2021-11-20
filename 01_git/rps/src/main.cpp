fd10154ff5f70755c71800b94199eb7a4ddcc723
Oleksandr Aleksieiev <oleksandr.aleksieiev@globallogic.com>
2021-11-21 01:00:38 +0200

    Task.01: Complete main
        Construct the game from parts
    
    Signed-off-by: Oleksandr Aleksieiev <oleksandr.aleksieiev@globallogic.com>

--- 01_git/rps/src/main.cpp
+++ 01_git/rps/src/main.cpp
@@ -5,7 +5,31 @@
 
 #include <iostream>
 
+#include "datatypes/constants.h"
+#include "driver/player_move.h"
+#include "ui/user_interface.h"
+
+using namespace rps;
+
 int main() {
-    std::cout << "bye" << std::endl;
+    char user_option = ' ';
+
+    while (user_option != EXIT_OPTION) {
+        UserInterface::printInviteMessage();
+        std::cin >> user_option;
+        if (user_option != EXIT_OPTION) {
+            auto user = PlayerMove(user_option);
+            if (user.isValid()) {
+                auto pc = PlayerMove();
+                pc.print("I choose ");
+                user.print("Your option is ");
+                UserInterface::printGameResult(pc == user);
+            } else {
+                UserInterface::printInvalidInputMessage();
+            }
+        }
+    }
+    UserInterface::printExitMessage();
+
     return 0;
 }
